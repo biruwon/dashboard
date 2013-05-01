254eee512b951cc2180bfb0934db089fd8e2e6e7
biruwon <ajesus@neventum.com>
2013-05-01 19:46:17 +0200

    Use of Twitter API with Guzzle

--- src/Biruwon/DashboardBundle/Controller/DefaultController.php
+++ src/Biruwon/DashboardBundle/Controller/DefaultController.php
@@ -3,11 +3,25 @@
 namespace Biruwon\DashboardBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Guzzle\Http\Client;
+use Guzzle\Plugin\Oauth\OauthPlugin;
 
 class DefaultController extends Controller
 {
     public function indexAction($name)
     {
         return $this->render('DashboardBundle:Default:index.html.twig', array('name' => $name));
+    }
+
+    public function twitterAction()
+    {
+    	$twitterClient = $this->container->get('guzzle.twitter.client');
+    	$status = $twitterClient->get('statuses/user_timeline.json')
+    		->send()->getBody();
+
+    	return $this->render('DashboardBundle:Default:index.html.twig', array(
+            'status' => $status
+    		)
+    	);
     }
 }
