f2ef62966d5125c029a102858873cecf6795c580
Biruwon <ajesus@grupeina.com>
2013-06-12 13:57:29 +0200

    Working on User, login and security

---
+++ src/Biruwon/DashboardBundle/Entity/Profile.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace Biruwon\DashboardBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * Profile
+ *
+ * @ORM\Table()
+ * @ORM\Entity
+ */
+class Profile
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var User
+     *
+     * @ORM\OneToOne(targetEntity="User", mappedBy="profile")
+     */
+    private $user;
+
+    /**
+     * @var Image
+     *
+     * @ORM\Column(name="image", type="object")
+     * @Assert\Image()
+     */
+    private $image;
+
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set user
+     *
+     * @param User $user
+     * @return Profile
+     */
+    public function setUser($user)
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+
+    /**
+     * Get user
+     *
+     * @return User
+     */
+    public function getUser()
+    {
+        return $this->user;
+    }
+
+    /**
+     * Set image
+     *
+     * @param \Image $image
+     * @return Profile
+     */
+    public function setImage($image)
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+
+    /**
+     * Get image
+     *
+     * @return \Image
+     */
+    public function getImage()
+    {
+        return $this->image;
+    }
+}
