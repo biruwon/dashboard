f73f53eb2540ccf50e62da075d0e0b337aa565a7
Biruwon <ajesus@grupeina.com>
2013-09-13 13:02:15 +0200

    Added twitter boostrap without bundles

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,8 +16,7 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-            new Biruwon\DashboardBundle\DashboardBundle(),
-            new Mopa\Bundle\BootstrapBundle\MopaBootstrapBundle()
+            new Biruwon\DashboardBundle\DashboardBundle()
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
