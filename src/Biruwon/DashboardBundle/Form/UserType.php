5f499a34c21508947606379bff38a434e3961335
Biruwon <ajesus@grupeina.com>
2013-09-16 18:20:58 +0200

    Added register form styles

--- src/Biruwon/DashboardBundle/Form/UserType.php
+++ src/Biruwon/DashboardBundle/Form/UserType.php
@@ -11,17 +11,45 @@ class UserType extends AbstractType
 	public function buildForm(FormBuilderInterface $builder, array $options)
 	{
 		$builder
-			->add('email', 'email', array('required' => true))
-			->add('username')
+			->add('email', 'email', array(
+				'required' => true,
+				'attr' => array(
+					'class' => 'form-control',
+					'placeholder' => 'john@company.com'
+					)
+				)
+			)
+			->add('username', 'text', array(
+				'required' => true,
+				'attr' => array(
+					'class' => 'form-control',
+					'placeholder' => 'UsEr745'
+					)
+				))
 			->add('password', 'repeated', array(
-				'first_options'  => array('label' => 'Password'),
-				'second_options' => array('label' => 'Repeat Password'),
+				'first_options'  => array(
+					'attr' => array(
+						'class' => 'form-control',
+						'placeholder' => 'mypass'
+						)
+					),
+				'second_options' => array(
+					'attr' => array(
+						'class' => 'form-control',
+						'placeholder' => 'mypass'
+						)
+					),
 				'invalid_message' => 'The password fields must match.',
 				'type' => 'password',
 				'required' => true
 				)
 			)
-			->add('create', 'submit')
+			->add('register', 'submit', array(
+				'attr' => array(
+					'class' => 'btn btn-primary'
+					)
+				)
+			)
 		;
 	}
 
