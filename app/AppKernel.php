5b74dbdb0e5461fccdd1962366113af6f4c91bc5
Biruwon <ajesus@grupeina.com>
2013-10-01 17:36:52 +0200

    Added redis

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,7 +16,8 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-            new Biruwon\DashboardBundle\DashboardBundle()
+            new Biruwon\DashboardBundle\DashboardBundle(),
+            new Snc\RedisBundle\SncRedisBundle()
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
